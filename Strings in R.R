25339aa08e6baf61faee0dfe930ae805b5ee8fba
Azmary <82352972+Azmary413@users.noreply.github.com>
2021-11-02 08:59:40 +0600

    Add files via upload

---
+++ Strings in R.R
@@ -0,0 +1,33 @@
+str1 = 'This is'
+str2 = 'Azmary'
+
+print(str2)
+print(class(str2))
+
+#length of string
+print(nchar(str1))
+
+#concatenation
+print(paste(str1,str2))
+
+#sub string
+result = substring('Stunning', 2,5)
+print(result)
+
+#change case
+result1 = tolower(str1)
+print(result1)
+
+result= toupper(str2)
+print(result)
+
+#Formation
+num = 193
+print(class(num))
+result2 = format(num)
+print(result2)
+print(class(result2))
+
+num1 = 193.902019
+Format1 = format(num1,digit = 5)
+print(Format1)
