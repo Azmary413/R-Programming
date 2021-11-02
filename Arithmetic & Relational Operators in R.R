6a2ae91972c064044af4eb414ccee6d8baad64e7
Azmary <82352972+Azmary413@users.noreply.github.com>
2021-11-02 09:00:05 +0600

    Add files via upload

---
+++ Arithmetic & Relational Operators in R.R
@@ -0,0 +1,29 @@
+#Arithmetic Operators
+x = 5
+y =6
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%%y)
+print(x^y)
+
+#In vector
+v1 = c(3,5,7,8)
+v2 = c(2,4,6,7)
+print(v1+v2)
+print(v1 - v2)
+print(v1*v2)
+print(v1/v2)
+print(v1%%v2)
+print(v1^v2)
+
+#Relational Operators
+v3 = c(3,5,7,8)
+v4 = c(2,6,7,9)
+print(v3 > v4)
+print(v3 < v4)
+print(v3 != v4)
+print(v3 <= v4)
+print(v3 >= v4)
