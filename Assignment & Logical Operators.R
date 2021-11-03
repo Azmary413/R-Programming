f888e683e3e3cc072a247ea54fc381b6dbc3f23e
Azmary <82352972+Azmary413@users.noreply.github.com>
2021-11-04 00:09:09 +0600

    Add files via upload

---
+++ Assignment & Logical Operators.R
@@ -0,0 +1,35 @@
+#Left Assignment Operators
+var1 = c(1,2,3)
+var2 <- c(2,4,5)
+print(var1)
+print(var2)
+
+#Right Assignment Operators
+c(5,4,6) -> var1
+c(3,54,65) ->> var2
+print(var1)
+print(var2)
+
+#Logical And Operators
+var1 = c(34,0,65)
+var2 <-c(0,5,3)
+print(var1 & var2)
+
+var1 = c(34,0,65)
+var2<-c(34,2,0)
+print(var1 && var2)
+
+#Logical OR Operators
+var1 = c(34,0,65)
+var2<- c(34,2,0)
+print(var1 || var2)
+
+var1 = c(34,0,0)
+var2<- c(34,2,0)
+print(var1 | var2)
+
+#Logical NOT Operators
+var1 = c(34,0,65)
+var2<- c(34,2,0)
+print(!var1)
+print(!var2)
